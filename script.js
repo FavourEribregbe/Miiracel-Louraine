72caad8c054308570235fea0d55cc79ec3bd0d41
Favour Eribregbe <leefavour61122@gmail.com>
2023-06-04 13:48:02 +0100

    fifth-phase

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let mynav = document.querySelector('.navbar');
 
 
 menubar.onclick = () =>{
-    menubar.classList.toggle('fa-regular fa-x')
+    menubar.classList.toggle('fa-times')
     mynav.classList.toggle('active')
 }
